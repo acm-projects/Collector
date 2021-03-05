0161fa9b6a03aef3bca79fdeb76cbe7817e72521
Micheal Kempner <mwk190000@utdallas.edu>
2021-03-05 14:37:41 -0600

    Edited Login.js and merged Wireframe
    Changed Login.js to generate a simple login page using Material UI and our color scheme. Not sure if it actually works lol

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,54 +1,155 @@
-import React , {useRef,useState} from "react"
-import { Form,Button,Card,Alert} from "react-bootstrap"
-import {useAuth} from '../contexts/AuthContext'
-import {Link, useHistory} from "react-router-dom"
+import React from 'react';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import Container from '@material-ui/core/Container';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { AppBar, Toolbar, IconButton, Icon } from '@material-ui/core';
+import { Menu, LockOutlined } from '@material-ui/icons';
 
-export default function Login() {
-    const emailRef=useRef()
-    const passwordRef=useRef()
-    const {login}=useAuth()
-    const [error,setError]=useState('')
-    const [loading,setLoading]=useState(false)
-    const history=useHistory()
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
 
-    async function handleSubmit(e){
-        e.preventDefault()
-    
-        try{
-            setError("")
-            setLoading(true)
-            await login(emailRef.current.value,passwordRef.current.value)
-            history.push("/")
-            console.log(error)
-        }catch{
-            setError('Failed to sign in')
-        }
-        setLoading(false)
-    }
 
-    return (
-        <>
-        <Card>
-            <Card.Body>
-              <h2 className="text-center mb-4">Log In</h2>  
-              {error && <Alert variant="danger">{error}</Alert>}
-              <Form onSubmit={handleSubmit}>
-                  <Form.Group id="email">
-                      <Form.Label>Email</Form.Label>
-                      <Form.Control type="email" ref={emailRef} required/>
-                  </Form.Group>
-                  <Form.Group id="password">
-                      <Form.Label>Password</Form.Label>
-                      <Form.Control type="password" ref={passwordRef} required/>
-                  </Form.Group>
-                  <Button disabled={loading} className="w-100" type="submit">Log In</Button>
-              </Form>
-            </Card.Body>
-            </Card>
-            <div className="w-100 text-center mt-2">
-                Need an Account? <Link to="/signup">Sign Up</Link>
+const theme = createMuiTheme(
+    {
+    palette: {
+      primary: {
+        light: '#5e677d',
+        main: '#333d51',
+        dark: '#0b1729',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ffdd5e',
+        main: '#d3ac2b',
+        dark: '#9e7d00',
+        contrastText: '#000',
+      },
+    },
+  });
+
+function Copyright() 
+  {
+      return (
+        <Typography variant="body2" color="textSecondary" align="center">
+          {'Copyright © '}
+          <Link color="inherit" href="https://material-ui.com/">
+            Your Website
+          </Link>{' '}
+          {new Date().getFullYear()}
+          {'.'}
+        </Typography>
+      );
+  }
+  
+const useStyles = makeStyles((theme) => ({
+      paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+      },
+      avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+      },
+      form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(1),
+      },
+      submit: {
+        margin: theme.spacing(3, 0, 2),
+      },
+    }));
+  
+    export default function SignIn() {
+        const classes = useStyles();
+      
+        return (
+          <Container component="main" maxWidth="xs">
+            <CssBaseline />
+            <div className={classes.paper}>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+                        <Menu />
+                    </IconButton>
+                    <Typography variant="h6" className={classes.title}>
+                    Collector
+                    </Typography>
+                    <Button color="inherit">Login</Button>
+                </Toolbar>
+            </AppBar>
+              <Avatar className={classes.avatar}>
+                <Icon>
+                    <LockOutlined />
+                /</Icon>
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <form className={classes.form} noValidate>
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  id="email"
+                  label="Email Address"
+                  name="email"
+                  autoComplete="email"
+                  autoFocus
+                />
+                <TextField
+                  variant="outlined"
+                  margin="normal"
+                  required
+                  fullWidth
+                  name="password"
+                  label="Password"
+                  type="password"
+                  id="password"
+                  autoComplete="current-password"
+                />
+                <FormControlLabel
+                  control={<Checkbox value="remember" color="primary" />}
+                  label="Remember me"
+                />
+                <Button
+                  type="submit"
+                  fullWidth
+                  variant="contained"
+                  color="primary"
+                  className={classes.submit}
+                >
+                  Sign In
+                </Button>
+                <Grid container>
+                  <Grid item xs>
+                    <Link href="#" variant="body2">
+                      Forgot password?
+                    </Link>
+                  </Grid>
+                  <Grid item>
+                    <Link href="#" variant="body2">
+                      {"Don't have an account? Sign Up"}
+                    </Link>
+                  </Grid>
+                </Grid>
+              </form>
             </div>
-        </>
-    )
-}
-
+            <Box mt={8}>
+              <Copyright />
+            </Box>
+          </Container>
+        );
+      }
+      
