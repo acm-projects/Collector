549a978ae8eb5e30325e770d599a2ed935ecc9b5
amoghyatnatti <amoghyatnatti@gmail.com>
2021-04-01 13:40:54 -0500

    Updated dev to resolve conflicts

--- src/components/App.js
+++ src/components/App.js
@@ -4,14 +4,6 @@ import {AuthProvider} from "../contexts/AuthContext"
 import {BrowserRouter as Router,Switch,Route} from "react-router-dom"
 import Dashboard from "./Dashboard"
 import Login from "./Login"
-<<<<<<< HEAD
-import Homepage from "./Homepage"
-import CreateListing from "./CreateListing"
-
-function App() { 
-  return (
-     <div>
-=======
 import LandingPage from "./LandingPage"
 import ProfilePage from './ProfilePage'
 import ForgotPassword from './ForgotPassword'
@@ -25,7 +17,6 @@ function App() {
     
      <div>
      <CssBaseline />
->>>>>>> frontend
        <Router>
        <AuthProvider>
          <Switch>
@@ -33,14 +24,10 @@ function App() {
            <Route path="/dashboard" component={Dashboard}/> 
            <Route path="/signup" component={Signup}/>
            <Route path="/login" component={Login}/>
-<<<<<<< HEAD
-           <Route path="/sell" component={CreateListing}/>
-=======
            <Route path="/landing" component={LandingPage}/>
            <Route path="/gallery" component={ProductGallery}/>
            <Route path="/forgotpassword" component={ForgotPassword}/>
            <Route path="/profile" component={ProfilePage}/>
->>>>>>> frontend
          </Switch>
        </AuthProvider>
        </Router>
